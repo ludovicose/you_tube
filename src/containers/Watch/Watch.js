aaece5218d07f2832c921df62e0555b1f8255eff
Aibek <aibek.muratbekov@gmail.com>
2020-02-25 17:33:58 +0600

    Horizontal Video Preview

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Watch.scss';
-import {Video} from "../../components/Video/Video";
+import {VideoPreview} from "../../components/VideoPreview/VideoPreview";
 
 export class Watch extends React.Component {
   render() {
-    return (
-     <div style={{maxWidth: '80%'}}>
-        <Video id='-7fuHEEmEjs'/>
-      </div>
+   return (
+      <React.Fragment>
+        <VideoPreview horizontal={true}/>
+        <VideoPreview />
+      </React.Fragment>
     );
   }
 }
