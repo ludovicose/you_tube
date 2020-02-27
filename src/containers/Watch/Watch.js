19a368c609eab81c4a55b168cd062a069500931f
Aibek <aibek.muratbekov@gmail.com>
2020-02-27 17:04:22 +0600

    Comments

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -4,6 +4,7 @@ import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 import {Video} from '../../components/Video/Video';
 import {VideoMetadata} from '../../components/VideoMetadata/VideoMetadata';
 import {VideoInfoBox} from '../../components/VideoInfoBox/VideoInfoBox';
+import {Comments} from '../Comments/Comments';
 
 export class Watch extends React.Component {
   // stretch?
@@ -13,7 +14,7 @@ export class Watch extends React.Component {
         <Video className='video' id='-7fuHEEmEjs'/>
         <VideoMetadata className='metadata' viewCount={1000}/>
         <VideoInfoBox className='video-info-box'/>
-        <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
+         <Comments className='comments'/>
         <RelatedVideos className='related-videos'/>
       </div>
     );
