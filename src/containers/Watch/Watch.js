a8e106defbe0f8b01295192ba29597f3acf19e35
Aibek <aibek.muratbekov@gmail.com>
2020-02-27 16:56:01 +0600

    Video Infobox

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -3,14 +3,16 @@ import './Watch.scss';
 import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 import {Video} from '../../components/Video/Video';
 import {VideoMetadata} from '../../components/VideoMetadata/VideoMetadata';
+import {VideoInfoBox} from '../../components/VideoInfoBox/VideoInfoBox';
 
 export class Watch extends React.Component {
+  // stretch?
   render() {
     return (
       <div className='watch-grid'>
         <Video className='video' id='-7fuHEEmEjs'/>
         <VideoMetadata className='metadata' viewCount={1000}/>
-        <div className='video-info-box' style={{width: '100%', height: '100px', background: '#BD10E0'}}>Video Info box</div>
+        <VideoInfoBox className='video-info-box'/>
         <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
         <RelatedVideos className='related-videos'/>
       </div>
