49fe3dffd809f73ff24208a92e7a8f397e7d7ce7
Aibek <aibek.muratbekov@gmail.com>
2020-02-28 16:36:08 +0600

    Adding Redux and redux-saga

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux'
 import {BrowserRouter} from "react-router-dom";
+import {configureStore} from "./store/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 
